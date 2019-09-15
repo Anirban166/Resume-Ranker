5fa02505325d9ead3e810839d3bb2c923071f202
Anirban Chetia <bloodraven166@gmail.com>
2019-09-15 21:11:12 +0530

    Add files via upload

---
+++ FileFilterer.java
@@ -0,0 +1,21 @@
+package anirban;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class FileFilterer implements FileFilter
+{
+    public final String[] AcceptedFileExtensions = new String[] {"doc","pdf"};
+	public boolean accept(File file)
+	  {
+	    for (String extension : AcceptedFileExtensions)
+	    {
+	      if (file.getName().toLowerCase().endsWith(extension))
+	      {
+	        return true;
+	      }
+	    }
+	    return false;
+	  }
+}
+	
