9f59e800ae3246295e8945d193bd2d31b27dc43e
Anirban166 <bloodraven166@gmail.com>
2022-05-18 02:39:59 -0700

    Started GUI and Scanner classes, wrote the main function and configured dependencies (using Maven).

---
+++ Source/Kotlin/GUI.kt
@@ -0,0 +1,14 @@
+import java.awt.Font
+import java.awt.Color
+import java.awt.TextArea
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JButton
+import javax.swing.JTextField
+import org.apache.log4j.BasicConfigurator
+
+class GUI(val onClickCallback: () -> Unit) : JFrame("Keyword-based Resume Scanner (V2)") 
+{
+    
+}
---
+++ Source/Kotlin/KeywordScanner.kt
@@ -0,0 +1,16 @@
+import java.util.*
+import java.io.File
+import java.io.FileFilter
+import java.io.FileWriter
+import java.io.FileInputStream
+import java.util.regex.Pattern
+import org.apache.pdfbox.Loader
+import org.apache.poi.hwpf.HWPFDocument
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.text.PDFTextStripper
+import org.apache.poi.hwpf.extractor.WordExtractor
+
+class ResumeScanner(private val gui: GUI) 
+{
+
+}    
