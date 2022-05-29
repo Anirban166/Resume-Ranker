342df87b9b3961b196b76af04f76e3f357dd4041
Anirban <bloodraven166@gmail.com>
2022-05-28 22:20:54 -0700

    Started writing code for supporting themes, added a material one that can switch in between light and dark themes (can extend these later to support more choices, but kinda settled with the palette configurations for these two standard ones after a bit of tinkering)

---
+++ Source/Main/Kotlin/Theme/Colors.kt
@@ -0,0 +1,10 @@
+package theme
+import androidx.compose.ui.graphics.Color
+
+val Teal200 = Color(0xFF03DAC5)
+val DarkGray = Color(0xFF141414)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val LightGray = Color(0xFFFCFCFC)
+val MediumGray = Color(0xFF9C9C9C)
